6e4af75df700090d98518d03323b87c7402a1c61
Tanzim <tanzim67@gmail.com>
2021-03-10 15:31:12 +0600

    29.Comments fetched need bugs fixed

--- forumapp/src/views/fullPost.vue
+++ forumapp/src/views/fullPost.vue
@@ -19,14 +19,14 @@
         </div>
          <h1 class="text-xl font-sans font-medium text-center p-5">Add a comment</h1>
           <div class="rounded-2xl border border-gray-300 bg-white border-box shadow-2xl ">
-                <textarea name="" v-model="comment" id="" cols="4" rows="4" class="border border-gray-200 w-full ">Hello</textarea>
+                <textarea name="" v-model="comment" id="" cols="4" rows="4" class="border border-gray-200 w-full "></textarea>
                 <div v-if='comment'>
                     <button @click.prevent="addComment" class="px-4 py-1 bg-blue-600 text-white font-sans">Comment</button>
                 </div>
           </div>
            <h1 class="text-xl font-sans font-medium text-center p-3 text-2xl">Comments</h1>
            <div class="space-y-2">
-                   <div class="rounded-2xl border border-gray-300 bg-white py-8 border-box shadow-2xl">
+                   <!-- <div class="rounded-2xl border border-gray-300 bg-white py-8 border-box shadow-2xl">
               <p class="font-serif px-4 text-md text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam expedita tempore, in quo libero adipisci fugit labore tenetur aliquid, porro eligendi cupiditate voluptatibus sapiente? Fuga ipsum debitis praesentium voluptatibus soluta!</p>
               <div class="xl:flex xl:flex-row sm:flex sm:flex-col justify-between w-11/12">
                   <div>
@@ -41,12 +41,12 @@
         
              
 
-          </div>
-          <div class="rounded-2xl border border-gray-300 bg-white py-8 border-box shadow-2xl">
-              <p class="font-serif px-4 text-md text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam expedita tempore, in quo libero adipisci fugit labore tenetur aliquid, porro eligendi cupiditate voluptatibus sapiente? Fuga ipsum debitis praesentium voluptatibus soluta!</p>
+          </div> -->
+          <div class="rounded-2xl border border-gray-300 bg-white py-8 border-box shadow-2xl" v-for="authUserComment in authUserComments" :key="authUserComment.id">
+              <p class="font-serif px-4 text-md text-black">{{authUserComment.content}}</p>
                   <div class="xl:flex xl:flex-row sm:flex sm:flex-col justify-between w-11/12">
                   <div>
-                          <p class="font-serif px-4 text-xl text-gray-600 py-1">Posted on 10/03/2020 by <a href=""></a> </p>
+                          <p class="font-serif px-4 text-xl text-gray-600 py-1">Posted on 10/03/2020 by <a href="">{{authUserComment.user_name}}</a> </p>
                   </div>
                   <div class="ml-4 xl:ml-0">
                       <button class="px-1 py-1 bg-green-300 text-white font-sans rounded-md"><span class="material-icons">create</span></button>
@@ -84,7 +84,8 @@ export default {
             id:this.$route.params.id,
             username:'',
             useremail:'',
-            postId:''
+            postId:'',
+            authUserComments:[],
             
         }
     },
@@ -111,10 +112,10 @@ export default {
 
     },
     created(){
-        console.log(this.id);
+        
         let user=auth.currentUser;
         db.collection("users")
-        .doc(user.uid)
+        .doc()
         .get()
         
         .then((doc)=>{
@@ -136,13 +137,28 @@ export default {
                data.id=doc.id;
             //    console.log(data.id);
                this.postId=data.id;
+               console.log(this.id);
               console.log(this.postId);
-            //    console.log(data.user_email);
-            //    this.useremail=data.user_email;
-            //    console.log(this.useremail);
+            
                
 
            }
+       })
+       //Comments 
+       const commenstRef=db.collection("comments");
+
+       commenstRef.onSnapshot((snap)=>{
+           snap.forEach(doc => {
+               this.authUserComments=[];
+
+
+               let data=doc.data();
+               if(this.id==data.post_id){
+                   this.authUserComments.push(data);
+               }
+               
+           });
+
        })
 
     }
