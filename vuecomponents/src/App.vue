c16b08dec0babb7adbb972cb823f3094b871e590
Tanzim <tanzim67@gmail.com>
2021-12-22 20:08:23 +0600

    1-Switch button

---
+++ vuecomponents/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <switchbutton />
+  
+</template>
+
+<script>
+
+import switchbutton from './components/MyComponents/switchbutton.vue'
+
+export default {
+  name: 'App',
+  components: {
+    switchbutton
+    
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vuecomponents/src/components/MyComponents/switchbutton.vue
@@ -0,0 +1,42 @@
+<template>
+  <button  @click="activateIsYes" class="bg-transparent bg-white border border-gray-600 h-6 rounded-full px-1 w-12">
+       <!-- <div class="bg-black h-4 w-4 border rounded-full ml-4"></div> -->
+       <!-- <div  class="bg-black h-4 w-4 border rounded-full ml-4"></div> -->
+       <div v-bind:class="[isYes ? 'bg-black h-4 w-4 border rounded-full ml-4': 'bg-black h-4 w-4 border rounded-full']">
+
+       </div> 
+       <div v-if="isYes">Paid</div>
+       <div v-else>Not Paid</div>
+  </button>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+export default {
+    // data(){
+    //     return{
+    //         isYes:false
+    //     }
+    // },
+    // methods:{
+    //     activateIsYes(){
+    //         this.isYes =! this.isYes
+    //     }
+    // }
+    //With Composition Api
+    setup(){
+        const isYes=ref(false)
+
+        const activateIsYes=()=>{
+          return isYes.value =! isYes.value
+        }
+
+        return {isYes,activateIsYes}
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
