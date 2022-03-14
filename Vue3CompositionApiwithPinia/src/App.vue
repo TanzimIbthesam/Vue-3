dfd9f5901df90c9deff2b49a9018f8c49bf17439
Tanzim <tanzim67@gmail.com>
2022-03-14 15:41:08 +0600

    1-Composition Api inbstal with Vite Counter app

---
+++ Vue3CompositionApiwithPinia/src/App.vue
@@ -0,0 +1,17 @@
+
+
+<template>
+  <!-- <HomeView /> -->
+
+  <RouterView />
+</template>
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+import HomeView from './views/HomeView.vue';
+</script>
+<style>
+@import '@/assets/base.css';
+
+
+</style>
---
+++ Vue3CompositionApiwithPinia/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button @click="increaseCounter">Increase Counter</button>
+    Counter-{{counter}}
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const counter=ref(0);
+const increaseCounter=()=>{
+      counter.value++
+  }
+</script>
+
+<style>
+
+</style>
