3169f84760d89f81ee281eae3bb9475a9e23a0d9
Tanzim <tanzim67@gmail.com>
2022-03-14 16:31:09 +0600

    2-2 way data binding,reactivedata

--- Vue3CompositionApiwithPinia/src/views/HomeView.vue
+++ Vue3CompositionApiwithPinia/src/views/HomeView.vue
@@ -1,18 +1,32 @@
 <template>
   <div>
+    <!-- Non reactive data -->
+    {{nonreacttitle}}
+    Title-{{counterData.title}}
     <button @click="increaseCounter">Increase Counter</button>
-    Counter-{{counter}}
+    Counter-{{counterData.counter}}
+    <!-- 2 way data binding -->
+    <input type="text" v-model="counterData.title" name="" id="">
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-const counter=ref(0);
+import { reactive, ref } from "vue";
+const counter=ref(0),title=ref("Counter Title")
+//Mot reactive data
+const nonreacttitle="Title non react"
 const increaseCounter=()=>{
-      counter.value++
+      // counter.value++
+      // title.value="Title changed"
+      counterData.counter++;
+      counterData.title="Title changed"
   }
+const counterData=reactive({
+    counter:0,
+    title:'Counter Title'
+})
 </script>
 
 <style>
-
+ 
 </style>
