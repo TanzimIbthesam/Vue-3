11dff1528ebc3c0596a33da42fd2952df57af9ff
Tanzim <tanzim67@gmail.com>
2022-03-26 22:02:05 +0600

    15-UseVue online

--- Vue3CompositionApiwithPinia/src/App.vue
+++ Vue3CompositionApiwithPinia/src/App.vue
@@ -9,19 +9,32 @@
     <Router-link :to="{name:'testroute'}">Testroute</Router-link>
     <Router-link :to="{name:'posts'}">Posts</Router-link>
     <Router-link :to="{name:'modals'}">Modals</Router-link>
-    Username-{{userData.name}}
-    email-{{userData.email}}
+    Username-{{userData.name}}|email-{{userData.email}}|onlinestatus:{{online}}
+   
+
+
+    
   <RouterView  />
 </template>
 <script setup>
-import { reactive,provide } from 'vue'
-
+import { reactive,provide, onMounted,ref } from 'vue'
+import { useOnline } from '@vueuse/core'
 import { RouterLink, RouterView } from 'vue-router'
+
 const userData=reactive({
   name:'Tanzim101',
   email:'tanzim@gmail.com'
 })
+
+
+onMounted(()=>{
+  
+  console.log("Unmounted");
+})
+
 provide('userData',userData)
+const online=useOnline()
+
 </script>
 <style>
 @import '@/assets/base.css';
