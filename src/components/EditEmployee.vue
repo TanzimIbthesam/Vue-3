a2e4df2e5ba6ff5910f08f5e109d6f10bf4712e7
TANZIM <tanzim67@gmail.com>
2022-08-27 01:02:07 -0700

    2-Edit Done

--- src/components/EditEmployee.vue
+++ src/components/EditEmployee.vue
@@ -1,16 +1,62 @@
 <template>
-    <div>
-         The props is-{{id}}
+    <div class="container">
+        <div class="row">
+            <form @submit.prevent="handleSubmit " >
+                  <div class="">
+                    <div class="">
+                    <label>Name</label>
+                   <input 
+                   v-model="name"
+                   class="form-control" type="text" name="" id="" />
+                   
+                    </div>
+                    <div class="">
+                        <label>Address</label>
+                   <input class="form-control" 
+                   v-model="location"
+                   type="text" name="" id="" />
+                    </div>
+                    
+                </div>
+                <button type="submit" class="btn btn-primary marginleft">Update</button>
+                </form>
+        </div>
     </div>
 </template>
 
 <script>
 import axios from 'axios'
 export default({
+    data(){
+        return {
+           name:"",
+           location:""
+        }
+    },
 props:['id'],
+methods:{
+    handleSubmit(){
+        let alldetails={
+            name:this.name,
+            location:this.location
+        }
+         axios.patch('http://localhost:3001/employees/'+this.id,alldetails)
+         .then(response=>{
+            console.log(response);
+         }).then(()=>{
+            this.$router.push('/')
+         }).catch((err)=>{
+            console.log(err);
+         })
+    }
+
+},
 mounted(){
     axios.get('http://localhost:3001/employees/'+this.id)
-    .then(response=>console.log(response.data))
+    .then(response=>{
+        this.name=response.data.name
+        this.location=response.data.location
+    })
     
 }
 })
