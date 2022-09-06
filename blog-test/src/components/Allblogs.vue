756f6c047c69aaf26d0e59828a5f88bafb67c421
TANZIM <tanzim67@gmail.com>
2022-09-06 16:10:30 +0600

    Allblogs fetched with comments

---
+++ blog-test/src/components/Allblogs.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex-grow-1">
+                            <div v-for="allblog in ALL_BLOGS_LIST" :key="allblog.id">
+                                <h3 class="mb-0">{{allblog.title}}</h3>
+                            <div class="subheading mb-3">{{allblog.created_by}}</div>
+                            <div v-for="all_blog in allblog.all_blogs" :key="all_blog.id">
+                                <p>{{all_blog.blog_detail}}</p>
+                            </div>
+                            </div>
+                            
+                            
+                        </div>
+</template>
+
+<script>
+    
+    export default {
+        props:['ALL_BLOGS_LIST']  
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- blog-test/src/views/HomeView.vue
+++ blog-test/src/views/HomeView.vue
@@ -4,89 +4,38 @@
         <LeftNavbar />
         
         <div class="container-fluid p-0">
-            <!-- About-->
-            <!-- <section class="resume-section" id="about">
-                <div class="resume-section-content">
-                    <h1 class="mb-0">
-                        Vue Test
-                        <span class="text-primary">MononSoft</span>
-                    </h1>
-                    <div class="subheading mt-3">
-                        Instructions:                        
-                    </div>
-                    <p class="lead mb-5">
-					<ul class="fa-ul mb-0">
-                        <li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                            Populate Left Menu under BLOG
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                            Get Detail of Selected Blog and Comments and view in main content area
-                        </li>
-						<li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                            Make a Modal for Comment on click New Comment Button
-                        </li>
-						<li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                            Receive Commenter Name, Comment and current date time for from Comment Submit
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                            Create or remove new comment and display
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                            Maintain SPA while develop
-                        </li>
-                    </ul>
-					<div class="subheading mt-5">
-                        API's:                        
-                    </div>
-                    <p class="lead mt-3">
-					<ul class="fa-ul mb-0">
-                        <li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                            ALL-BLOGS [get] ["http://vue-test.gingerbd.com/api/all-blogs"]
-                        </li>
-						<li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                            LEFT-MENU [get] ["http://vue-test.gingerbd.com/api/blogs"]
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                            VIEW-BLOG [get] ["http://vue-test.gingerbd.com/api/view-blog/id"]
-                        </li>
-						<li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                            SAVE COMMENT [post] ["http://vue-test.gingerbd.com/api/post-comment?blog_id=blog_id&comment=comment&comment_datetime=now()&user=user"]
-                        </li>
-						<li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                            DELETE COMMENT [get] ["http://vue-test.gingerbd.com/api/remove-comment/id"]
-                        </li>
-                    </ul>
-					</p>
-					
-					</p>
-                    
-                </div>
-            </section> -->
+           
             <Instructions />
             <hr class="m-0" />
             <!-- Experience-->
             <section class="resume-section" id="experience">
                 <div class="resume-section-content">                    
                     <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
-                        <div class="flex-grow-1">
-                            <h3 class="mb-0">Blog Title</h3>
-                            <div class="subheading mb-3">Blog Author</div>
-                            <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
-                        </div>
-                        <div class="flex-shrink-0"><span class="text-primary">11 March 2013 : 10am</span></div>
+                         <div class="flex-grow-1">
+                            <div v-for="allblog in allblogs" :key="allblog.id">
+                                <h3 class="mb-0">{{allblog.title}}</h3>
+                            <div class="subheading mb-3">{{allblog.created_by}}</div>
+                            <div v-for="all_blog in allblog.all_blogs" :key="all_blog.id">
+                                <p>{{all_blog.blog_detail}}</p>
+                            <!-- <Allblogs :ALL_BLOGS_LIST="allblogs" /> -->
+                        <div class="flex-shrink-0"><span class="text-primary">{{all_blog.blog_datetime}}</span></div>
                     </div>
-					<div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div 
+                    v-for="all_blog_comment in allblog.all_comments" :key="all_blog_comment.id"
+                    class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h5 class="mb-0"><hr>Comments</h5>
+                            
+                            
+							<div class="subheading mb-1" style="font-size:11pt;"><i class="fa fa-user"></i>{{all_blog_comment.user}}<div class="float-right"><i class="fa fa-clock"></i>{{all_blog_comment.comment_datetime}}</div></div>
+                            <p>{{all_blog_comment.comment}}</p>							
+							<div class="subheading mb-1"><button class="btn btn-sm btn-info">New Comment</button></div>
+                        </div>                        
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+					<!-- <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
                         <div class="flex-grow-1">
                             <h5 class="mb-0">Blog Comment<hr></h5>
                             <div class="subheading mb-1" style="font-size:11pt;"><i class="fa fa-user"></i> Commenter 1 <div class="float-right"><i class="fa fa-clock"></i> 2021-03-23 10.05 pm</div></div>
@@ -95,12 +44,12 @@
                             <p>Comment 2</p>							
 							<div class="subheading mb-1"><button class="btn btn-sm btn-info">New Comment</button></div>
                         </div>                        
-                    </div>
+                    </div> -->
                     
                 </div>
             </section>
 
-            <!-- <div v-for="blg in allBlogs" ><Holder key="blg.id" :blg="blg" />            
+             <!-- <div v-for="blg in allBlogs" ><Holder key="blg.id" :blg="blg" />            
             </div>                  -->
         </div>
     </div>
@@ -113,12 +62,26 @@ import LeftNavbar from "../components/LeftNavbar.vue";
 import Instructions from "@/components/Instructions.vue";
 // @ is an alias to /src
 
-
+import axios from 'axios'
+import Allblogs from "@/components/Allblogs.vue";
 export default {
   name: 'HomeView',
   components: {
     LeftNavbar,
-    Instructions
+    Instructions,
+    Allblogs
+},
+data(){
+    return{
+        allblogs:[]
+
+    }
+},
+created(){
+    axios.get('http://vue-test.gingerbd.com/api/all-blogs')
+     .then(res=>this.allblogs=res.data)
+    // .then(res=>console.log(res.data))
+
 }
 }
 </script>
